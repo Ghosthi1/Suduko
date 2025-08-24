906351dc6a55e8ad7f14df8ee7ab00b548dc9c4c
ghosthi1 <c-forsyth02@sky.com>
2025-08-24 23:23:26 +0100

    using vect

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::io;
 
 fn main() {
-    let gridSize = getGridSize();
-    generateSudokuGrid(gridSize);
+    let grid_size = get_grid_size();
+
+    // creates a dynamic vector 
+    let grid: Vec<Vec<i32>> = vec![vec![0; grid_size as usize]; grid_size as usize];
+    
+    // initializes the Sudoku grid displaying blank values
+    generate_sudoku_grid_init(&grid);
 }
 
-fn getGridSize() -> i32 {
+// Gets grid from user
+fn get_grid_size() -> i32 {
     println!("Enter the size of the Sudoku grid (e.g., 9 for a 9x9 grid):");
 
     //sets a mutable string 
@@ -26,6 +32,8 @@ fn getGridSize() -> i32 {
 
 }
 
-fn generateSudokuGrid(gridSize: i32) {
+//displays the grid to user 
+fn generate_sudoku_grid_init(grid_size: &Vec<Vec<i32>>) {
 
 }
+
