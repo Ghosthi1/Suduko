f5bf5fe40d37853e0e2392354e680144984b902a
ghosthi1 <c-forsyth02@sky.com>
2025-08-25 14:51:40 +0100

    added draw box button

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,58 @@
-use std::io;
+use eframe::egui;
 
-fn main() {
-    let grid_size = get_grid_size();
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
-    // creates a dynamic vector 
-    let grid: Vec<Vec<i32>> = vec![vec![0; grid_size as usize]; grid_size as usize];
+    // Configure the native window options
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(750.0, 500.0)), // Set window size to 1000x1000 pixels
+        ..Default::default()
+    };
+
+    // eframe::run_native creates and runs a native desktop application
+    eframe::run_native(
+        "Suduko",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())), // _cc is the creation context, contains OpenGL context, etc.
+    )
+}
+
+// MyApp struct holds the application state
+struct MyApp {
+    board: [[u8; 9]; 9],
+}
+
+// Initializes the struct variables
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            board: [[0; 9]; 9],
+        }
+    }
+}
+
+// Implement the eframe::App trait for MyApp
+// This trait defines how the app should update and render
+impl eframe::App for MyApp {
+    // update() is called every frame - this is where you define your UI
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // 1. Create panels/windows
+        egui::CentralPanel::default().show(ctx, |ui| {
+            
+            // 2. Handle user input
+            if ui.button("Reset").clicked() {
+                self.board = [[0; 9]; 9];  // Modify app state
+            }
+
+            // Draws the board
+            if ui.button("Draw Board").clicked() {
+                draw_board();
+            }
+
+        });
+    }
+}
+
+fn draw_board(){
     
-    // initializes the Sudoku grid displaying blank values
-    generate_sudoku_grid_init(&grid);
 }
-
-// Gets grid from user
-fn get_grid_size() -> i32 {
-    println!("Enter the size of the Sudoku grid (e.g., 9 for a 9x9 grid):");
-
-    //sets a mutable string 
-    let mut input = String::new();
-
-    io::stdin()
-        //reads line and appends to string ,&mut creates mutable reference
-        .read_line(&mut input)
-        // error handling
-        .expect("Failed to read line");
-
-    let number: i32 = input
-        .trim() //removes whitespace
-        .parse() //parses string to number
-        .expect("Please type a number!"); //error handling
-
-    return number;
-
-}
-
-//displays the grid to user 
-fn generate_sudoku_grid_init(grid_size: &Vec<Vec<i32>>) {
-
-}
-
